99bccce907f3855225810306e67dae7f395ff3a3
anyat <anyatarara93@gmail.com>
2025-04-09 23:26:40 +0200

    HW17

--- src/test/java/com/ait/qa55/HW13.java
+++ src/test/java/com/ait/qa55/HW13.java
@@ -56,22 +56,60 @@ public class HW13 {
         //contains
         driver.findElement(By.cssSelector("[href*='/books']"));
         driver.findElement(By.cssSelector("[href*='/apparel']"));
+        //href="/digital-downloads"
+        driver.findElement(By.cssSelector("[href*='downloads']"));
         //start->^
         driver.findElement(By.cssSelector("[href^='/gift']"));
         driver.findElement(By.cssSelector("[href^='/digital']"));
         //end on->$
-        //href="/digital-downloads"
-//        driver.findElement(By.cssSelector("[href$='/downloads']"));
         driver.findElement(By.cssSelector("[href$='/shoes']"));
-
-
-
-
-
-
-
     }
 
+    @Test
+    public void findElementByXpath(){
+        //xpath -> tag
 
+
+        //id->xpath -> //*[@id='value']
+        //driver.findElement(By.id("bar-notification"));
+        driver.findElement(By.xpath("//div[@id='bar-notification']"));
+        driver.findElement(By.xpath("//input[@id='small-searchterms']"));
+        driver.findElement(By.xpath("//input[@id='newsletter-subscribe-button']"));
+
+        //className-> //*[@class='value']
+        //driver.findElement(By.className("master-wrapper-main"));
+        driver.findElement(By.xpath("//div[@class='master-wrapper-main']"));
+        //driver.findElement(By.className("footer"));
+        driver.findElement(By.xpath("//div[@class='footer']"));
+
+        //contains-> //*[@contains(.,'Text')] частичное совпадение
+        driver.findElement(By.xpath("//h2[contains(.,'Welcome')]"));
+        //equal-> //*[text()='Text')] точное совпадение, оба варианта
+        driver.findElement(By.xpath("//h3[text()='Customer service']"));
+        driver.findElement(By.xpath("//h3[.='Information']"));
+
+        //starts-with-> //*[starts-with(@attr,'StartText')]
+        driver.findElement(By.xpath("//input[starts-with(@autocomplete,'of')]"));
+        driver.findElement(By.xpath("//div[starts-with(@title,'Err')]"));
+
+        //move up
+        driver.findElement(By.xpath("//a[@class='nivo-imageLink']/.."));
+        driver.findElement(By.xpath("//ul[@class='top-menu']/.."));
+
+        // parent
+        driver.findElement(By.xpath("//a[@class='ico-register']/parent::*"));
+        driver.findElement(By.xpath("//input[@class='button-1 search-box-button']/parent::form"));
+        driver.findElement(By.xpath("//a[@class='ico-register']/.."));
+
+       //ancestor
+        driver.findElement(By.xpath("//a[@class='ico-register']/ancestor::*")); //all
+        driver.findElement(By.xpath("//a[@class='ico-register']/ancestor::div")); //five options
+        driver.findElement(By.xpath("//a[@class='ico-register']/ancestor::div[4]")); //one option
+
+//        //child
+        driver.findElement(By.xpath("//div[@class='leftside-3']/following-sibling::div"));
+        driver.findElement(By.xpath("//div[@class='nivo-caption']/following-sibling::div"));
+
+    }
 
 }
