a2f3c0b69cdbea5f0fa4d9df76a66f006efac316
anyat <anyatarara93@gmail.com>
2025-04-09 23:26:07 +0200

    Lesson17

--- src/test/java/com/ait/qa55/FindElementTests.java
+++ src/test/java/com/ait/qa55/FindElementTests.java
@@ -73,8 +73,47 @@ public class FindElementTests {
         driver.findElement(By.cssSelector("[href^='/reg']"));
         //end on->$
         driver.findElement(By.cssSelector("[href$='/search']"));
+        //tag+id
+        driver.findElement(By.cssSelector("input#city"));
+        driver.findElement(By.cssSelector("div.mobile-header"));
+        driver.findElement(By.cssSelector("div.search-card"));
+        //tag+id+[attr='value']
+        driver.findElement(By.cssSelector("input#city[type='text']"));
+    }
 
+    //       //*[@att='value']
+    @Test
+    public void findElementByXpath(){
+        //xpath -> tag
+        //driver.findElement(By.tagName("h1"));
+        driver.findElement(By.xpath("//h1"));
 
+        //id->xpath -> //*[@id='value']
+        //driver.findElement(By.id("city"));
+        driver.findElement(By.xpath("//input[@id='city']"));
+
+        //className-> //*[@class='value']
+        //driver.findElement(By.className("header"));
+        driver.findElement(By.xpath("//div[@class='header']"));
+        //contains-> //*[@contains(.,'Text')] частичное совпадение
+        driver.findElement(By.xpath("//h2[contains(.,'Yalla')]"));
+        //equal-> //*[text()='Text')] точное совпадение, оба варианта
+        driver.findElement(By.xpath("//h2[text()='Type your data and hit Yalla!']"));
+        driver.findElement(By.xpath("//h2[.='Type your data and hit Yalla!']"));
+        //starts-with-> //*[starts-with(@attr,'StartText')]
+        driver.findElement(By.xpath("//label[starts-with(@for,'ci')]"));
+        //move up / вверх
+        driver.findElement(By.xpath("//a[@class='navigation-link']/.."));
+        // parent
+        driver.findElement(By.xpath("//h1/parent::*"));
+        driver.findElement(By.xpath("//h1/parent::div"));
+        driver.findElement(By.xpath("//h1/.."));
+        //ancestor предок
+        driver.findElement(By.xpath("//h1/ancestor::*")); //all
+        driver.findElement(By.xpath("//h1/ancestor::div")); //two options
+        driver.findElement(By.xpath("//h1/ancestor::div[2]")); //one option
+        //child потомок текущего узла
+        driver.findElement(By.xpath("//h1/following-sibling::form"));
     }
 
 
